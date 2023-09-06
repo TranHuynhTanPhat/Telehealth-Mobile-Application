7280ce01d19d2648eaf5851ed9d0caa9e6430faf
TranHuynhTanPhat <tranhuynhtanphat9380@gmail.com>
2023-09-06 20:34:03 +0700

    test ci

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:healthline/app/app_routes.dart';
 import 'package:healthline/app/healthline_app.dart';
@@ -22,9 +23,9 @@ void main() {
     expect(find.text('1'), findsNothing);
 
 
-    // // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
 
     // // Verify that our counter has incremented.
     // expect(find.text('0'), findsNothing);
