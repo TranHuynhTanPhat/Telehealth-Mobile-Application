a742a4d96b3cefd8fcc2241de1ec67cb5a3249e1
TranHuynhTanPhat <tranhuynhtanphat9380@gmail.com>
2023-09-06 20:27:45 +0700

    test ci

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -18,9 +18,9 @@ void main() {
     await tester.pumpWidget( MyApp(appRoute: AppRoute(),));
     
     
-    // // Verify that our counter starts at 0.
-    // expect(find.text('0'), findsOneWidget);
-    // expect(find.text('1'), findsNothing);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
     // // Tap the '+' icon and trigger a frame.
     // await tester.tap(find.byIcon(Icons.add));
